52cf8cb4b978f34395a410ddd035a1ad8a90825a
jchahin <jacobchahinb@gmail.com>
2025-08-24 02:24:14 -0500

    tarjeta jacob react

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './index.css'
+
+function App() {
+  return (
+    <div className="card">
+      <h1>Atala Jacob Chahin</h1>
+      <h2>Diseñador UX en formacion</h2>
+    </div>
+  )
+}
+
+export default App
